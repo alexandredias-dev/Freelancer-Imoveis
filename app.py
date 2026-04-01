d774f50b50de40558a915897579c381305a6b61f
Alexandre Dias Dev <alexandredias.dev@gmail.com>
2026-04-01 14:50:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,9 @@ from selenium.webdriver.common.by import By
 from datetime import datetime
 import openpyxl
 
-#1- Entrar no site https://www.imoveismartinelli.com.br/pesquisa-de-imoveis/?locacao_venda=V&id_cidade%5B%5D=41&finalidade=&dormitorio=&garagem=&vmi=&vma=&ordem=4
-
-
 driver = webdriver.Chrome()
 driver.get("https://www.imoveismartinelli.com.br/pesquisa-de-imoveis/?locacao_venda=V&id_cidade%5B%5D=41&finalidade=&dormitorio=&garagem=&vmi=&vma=&ordem=4")
 
-#2- Inserir o preço, link da casa e data dentro da planilha
 precos = driver.find_elements(By.XPATH,"//div[@class='card-valores']/div")
 
 links = driver.find_elements(By.XPATH,"//a[@class='carousel-cell is-selected']")
